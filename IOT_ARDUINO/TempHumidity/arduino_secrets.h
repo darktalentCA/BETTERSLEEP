f8797247732b575cd2f1483f90552ab21b990ca7
Wei Yan <weiyan@Weis-MacBook-Pro.local>
2023-10-14 20:21:38 -0400

    remove adafuit certs

--- IOT_ARDUINO/TempHumidity/arduino_secrets.h
+++
@@ -1,7 +0,0 @@
-#define SECRET_SSID "BELL833"
-#define SECRET_PASS "61D97A617A34"
-
-#define AIO_SERVER "io.adafruit.com"
-#define AIO_SERVERPORT 1883 // use 8883 for SSL
-#define AIO_USERNAME "darktalent" // This is from “key” access window in Section 5
-#define AIO_KEY "aio_PJjO57TmCUN3YW7EoMZvpYbHUItU" // This is from “key” access window in Section 5
